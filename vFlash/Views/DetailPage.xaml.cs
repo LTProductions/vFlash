27a52e65b75efaa4ddb9701ed113be71a8659a3a
LTProductions <latomaine@live.com>
2016-04-14 19:34:28 -0400

    Initial Commit.

---
+++ vFlash/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using vFlash.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace vFlash.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
