487c89374f6b8886ae685addae347d64b42af901
LTProductions <latomaine@live.com>
2016-05-30 22:15:47 -0400

    Started adding Quiz functionality; fixed invalid cast exception when reloading TextBoxString lists.

---
+++ vFlash/Models/QuizModel.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vFlash.Models
+{
+    public class QuizModel : BaseModel
+    {
+
+        private int _id;
+        public int ID
+        {
+            get { return _id; }
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private string _question;
+        public string Question
+        {
+            get { return _question; }
+            set
+            {
+                if (_question != value)
+                {
+                    _question = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private string _a;
+        public string A
+        {
+            get { return _a; }
+            set
+            {
+                if (_a != value)
+                {
+                    _a = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private string _b;
+        public string B
+        {
+            get { return _b; }
+            set
+            {
+                if (_b != value)
+                {
+                    _b = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private string _c;
+        public string C
+        {
+            get { return _c; }
+            set
+            {
+                if (_c != value)
+                {
+                    _c = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private string _d;
+        public string D
+        {
+            get { return _d; }
+            set
+            {
+                if (_d != value)
+                {
+                    _d = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ vFlash/Utils/Randomizer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using vFlash.Utils;
+
+namespace vFlash.Utils
+{
+    public static class Randomizer
+    {
+        public static IEnumerable<T> Shuffle1<T>(this IEnumerable<T> source)
+        {
+            Random random = new Random();
+            T[] copy = source.ToArray();
+
+            for (int i = copy.Length - 1; i >= 0; i--)
+            {
+                int index = random.Next(i + 1);
+                yield return copy[index];
+                copy[index] = copy[i];
+            }
+        }
+
+
+        public static int RandomNumber()
+        {
+            var rnd = new Random(DateTime.Now.Millisecond);
+            return 1;
+        }
+    }
+}
